a349b041aaec46e5417a7871c9225a70e40ac731
Alexander Sfakianos <43277245+asfakianos@users.noreply.github.com>
2018-12-03 09:25:47 -0500

    Update LEDRoomDisplay.ino

--- LEDRoomDisplay.ino
+++ LEDRoomDisplay.ino
@@ -1,11 +1,4 @@
 // Using:          https://www.adafruit.com/product/420
-// Github Library: https://github.com/pkourany/RGBmatrixPanel_IDE
-// Gibhub Example: https://github.com/pkourany/RGBmatrixPanel_IDE/blob/master/examples/scrolltext_16x32/scrolltext_16x32.ino
-// File API:       https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
-// ^ ^ Use with:   https://reactjs.org/docs/higher-order-components.html
-
-// Token = “e94ca7a33c00354cb20b43878bdfc010a3017a42”
-// ID = “2d0046000747363339343638”
 
 /******************************************************************************
  *                                                                            *
@@ -21,29 +14,28 @@
 #include <Adafruit_mfGFX.h>   // Core graphics library
 #include <RGBmatrixPanel.h>   // Hardware-specific library
 #include "math.h"
-// RGB Shield Version
-#define RGBSHIELDVERSION 4
 
 // Pin layout for our sensor. These should work despite the analog I/O as we let them function as on/off only
 #define TRIG A5
 #define ECHO A4
 
 // Pin layout for LED matrix
-#define CLK D6
-#define OE  D7
-#define LAT TX
-#define A   A0
-#define B   A1
-#define C   A2
-#define D   RX
+#define CLK	D6
+#define OE	D7
+#define LAT	A3
+#define A  	A0
+#define B  	A1
+#define C  	A2
 
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);     // Defining the matrix
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);     // Defining the matrix
 
 bool occupied = false;                      // Whether the room is occupied or not
 int textX = matrix.width();                 // The width of the LED matrix
 int textY = matrix.height();                // The height of the LED matrix
 int status = 0;                             // Will be published to the Particle website + used for LED text: 0 = vacant, 1 = occupied
 int numPassed = 0;                          // Tracking the total number passed (potentially deprecated?)
+uint16_t red =  0x1000;
+uint16_t green = 0x0080;
 
 static uint16_t boardColor = 0xFFFF;        // Color of text on the board
 
@@ -53,31 +45,40 @@ void setup() {
     pinMode(ECHO, INPUT);
 
     // LED Matrix
-    matrix.fillScreen(0);                   // Clears the setting.
-    matrix.setTextSize(2);                  // Look into appropriate size...
-    matrix.print("Setting up");          // Writes str to board
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    matrix.setCursor(1, 0);
+    matrix.setTextSize(1);
+    matrix.setTextColor(green);
+    matrix.print("Occupied");
     
-    Particle.variable("Status = ", status);       // Status of the room.
-    Particle.variable("Color=", boardColor);                             // Color of the board.
+    Particle.variable("Occupied= ", occupied);                 // Status of the room.
     Particle.variable("Num pass=", numPassed);
 }
 
 
 /* Checks if there is an object closer to the sensor than the door frame.
- * Returns true if the object is in the way, false if there is no interference. */
-bool check() {
-    unsigned long duration;                 // Keeps track of the length of the pulse
-    unsigned long distance;                 // Distance that the sensor captured in meters.
-    unsigned int across = 2;                // Distance from sensor to doorframe (SHORTER RATHER THAN LONGER)
+ * Returns true if the object is in the way, false if there is no interference.
+ * Takes maxDist in inches as the maximum distance that a player can be at. */
+bool check(uint32_t maxDist) {
 
-    // Checking the sensor for activity
-    duration = pulseIn(TRIG, HIGH);         // Gets the pulse length in ms
-    distance = duration * 0.343;            // ms -> m using 0.343 as m/ms speed of sound
+    uint32_t duration, inches, cm;
 
-    if(distance < across)
+    digitalWriteFast(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWriteFast(TRIG, LOW);
+
+    duration = pulseIn(A4, HIGH);
+
+    // Speed of Sound = 1130 ft/s (~ 74 in/s).
+    inches = duration / 74 / 2;
+
+    if (inches < maxDist)
         return true;
 
-    return false;
+    if (inches >= maxDist)
+        return false;
+
+
 }
 
 
@@ -93,7 +94,7 @@ void boardWrite(uint16_t color) {
 }
 
 
-void full() {
+void fill() {
     status = 1;                // Changing the status of the board & particle.var
     boardColor = 0x1000;       // Changing the color of the board to red (I think)
     occupied = true;
@@ -118,12 +119,9 @@ void loop() {
     
     // If occupied and there is no longer someone in there, it remains occupied
     else if (!occupied && result)
-        full();
+        fill();
 
-
-    // Refreshes every loop...
-    boardWrite(boardColor);
-    delay(50);                              // Doesn't need to accomodate sensors as pulseIn() does it automatically.
+    delay(10);                              // Doesn't need to accomodate sensors as pulseIn() does it automatically.
 }
 
 
